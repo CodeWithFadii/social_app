cb854f84975bc3cdfc1cc31650f8ae0143fd1fae
CodeWithFadii <115652088+CodeWithFadii@users.noreply.github.com>
2025-10-11 14:35:34 +0500

    new assets added

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -2,12 +2,16 @@ class AppAssets {
   static const String _imagePath = 'assets/images';
   static const String _iconPath = 'assets/icons';
 
+  // Images
   static const String authMap = '$_imagePath/auth_map.png';
   static const String stackCards = '$_imagePath/stack_cards.png';
   static const String welcomeBg = '$_imagePath/welcome_bg.jpg';
   static const String welcomePersons = '$_imagePath/welcome_persons.webp';
   static const String authCoins = '$_imagePath/auth_coins.png';
+  static const String bgExample1 = '$_imagePath/bg_example.1.jpg';
+  static const String personImage = '$_imagePath/person.jpg';
 
+  // Icons
   static const String google = '$_iconPath/google.svg';
   static const String apple = '$_iconPath/apple.svg';
   static const String backButton = '$_iconPath/back_button.svg';
@@ -16,4 +20,56 @@ class AppAssets {
   static const String mail = '$_iconPath/mail.svg';
   static const String person = '$_iconPath/person.svg';
   static const String otpLoader = '$_iconPath/otp_loader.svg';
+
+  // New Icons
+  static const String acRepair = '$_iconPath/ac_repair.svg';
+  static const String activity = '$_iconPath/activity.svg';
+  static const String activity1 = '$_iconPath/activity_1.svg';
+  static const String agreement = '$_iconPath/agreement.svg';
+  static const String attachment = '$_iconPath/attachment.svg';
+  static const String bug = '$_iconPath/bug.svg';
+  static const String calender = '$_iconPath/calender.svg';
+  static const String call = '$_iconPath/call.svg';
+  static const String camera = '$_iconPath/camera.svg';
+  static const String carpenter = '$_iconPath/carpenter.svg';
+  static const String caseIcon = '$_iconPath/case.svg';
+  static const String chatCamera = '$_iconPath/chat_camera.svg';
+  static const String clock = '$_iconPath/clock.svg';
+  static const String country = '$_iconPath/country.svg';
+  static const String coupen = '$_iconPath/coupen.svg';
+  static const String delete = '$_iconPath/delete.svg';
+  static const String dubai = '$_iconPath/dubai.svg';
+  static const String edit = '$_iconPath/edit.svg';
+  static const String edit1 = '$_iconPath/edit_1.svg';
+  static const String electrition = '$_iconPath/electrition.svg';
+  static const String feedback = '$_iconPath/feedback.svg';
+  static const String filter = '$_iconPath/filter.svg';
+  static const String help = '$_iconPath/help.svg';
+  static const String help1 = '$_iconPath/help_1.svg';
+  static const String home = '$_iconPath/home.svg';
+  static const String language = '$_iconPath/language.svg';
+  static const String list = '$_iconPath/list.svg';
+  static const String logout = '$_iconPath/logout.svg';
+  static const String marker = '$_iconPath/marker.svg';
+  static const String membership = '$_iconPath/membership.svg';
+  static const String order = '$_iconPath/order.svg';
+  static const String password = '$_iconPath/password.svg';
+  static const String play = '$_iconPath/play.svg';
+  static const String plumbing = '$_iconPath/plumbing.svg';
+  static const String posts = '$_iconPath/posts.svg';
+  static const String profile = '$_iconPath/profile.svg';
+  static const String recent = '$_iconPath/recent.svg';
+  static const String reminder = '$_iconPath/reminder.svg';
+  static const String search = '$_iconPath/search.svg';
+  static const String send = '$_iconPath/send.svg';
+  static const String services = '$_iconPath/services.svg';
+  static const String setting = '$_iconPath/setting.svg';
+  static const String star = '$_iconPath/star.svg';
+  static const String sticker = '$_iconPath/sticker.svg';
+  static const String theme = '$_iconPath/theme.svg';
+  static const String transaction = '$_iconPath/transaction.svg';
+  static const String verify = '$_iconPath/verify.svg';
+  static const String wallet = '$_iconPath/wallet.svg';
+  static const String wallet1 = '$_iconPath/wallet_1.svg';
+  static const String work = '$_iconPath/work.svg';
 }
